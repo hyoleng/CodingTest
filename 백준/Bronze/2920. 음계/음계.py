1f7708bb1239857e487c3225da70a6abecdaa792
이효원 <hyoleng@gmail.com>
2025-03-21 01:57:22 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,19 +1,7 @@
 A = list(map(int, input().split()))
-if A[0] == 1:
-    for i in range(1, 8):
-        if A[i] == i+1:
-            pass
-        else:
-            print('mixed')
-            exit()
+if A == sorted(A):
     print('ascending')
-elif A[0] == 8:
-    for i in range(1, 8):
-        if A[i] == A[0]-i:
-            pass
-        else:
-            print('mixed')
-            exit()
+elif A == sorted(A, reverse = True):
     print('descending')
 else:
     print('mixed')
